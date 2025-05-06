62c40690efe9f8ea34d59ebb2293c22260ed1591
Jyri Oksanen <jyri@nesu.org>
2025-05-06 18:50:59 +0300

    Fix merge conflicts

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-
-print("Hello world!")
-<<<<<<< HEAD
-=======
 print("Hello world!")
 print("Hello darkness my old friend")
->>>>>>> upstream/master
-=======
 print("I've come to talk with you again")
->>>>>>> upstream/feature/modify-print
